a82ff224d5b961aa6514da7edb512aac5592c6e2
Markus Hällfors <markus@hallfors.com>
2018-08-14 23:00:12 +0300

    Add folders. Prepare for recursive folder syncing.

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::path::Path;
 
 use filetime::FileTime;
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 struct SyncFile {
     file_name: OsString,
     path: OsString,
@@ -28,6 +28,7 @@ enum ActionType {
 struct DiffFile<'a> {
     file: &'a SyncFile, 
     action: ActionType,
+    is_directory: bool,
 }   
 
 fn main() {
@@ -48,59 +49,21 @@ fn main() {
     let source_files: Vec<SyncFile> = read_files(source);
     let mut destination_files: Vec<SyncFile> = read_files(destination);
 
+    // Vector that stores the paths to the folders that have to be still looked
+    let mut folders_todo: Vec<OsString> = Vec::new();
+
     // Stores the information about what actions needs to be done later
     let mut diff_files: Vec<DiffFile> = Vec::new();
 
-    // Check the differences between the source folder and the destination
-    for file in &source_files {
-
-        let mut file_exists_already = false;
-        let mut dest_file_index = 0;
-
-        println!("Checking file: {:?}", &file.path);
-
-        // Check if the same file is already in destination
-        for (i, dest_file) in destination_files.iter().enumerate() {
-        //for dest_file in &destination_files {
-            if dest_file.file_name == file.file_name {
-
-                file_exists_already = true;
-
-                // check the modified times and sizes
-                if dest_file.size == file.size && dest_file.modified == file.modified {
-                    println!("No changes needed: {:?}", &dest_file.path);
-                    dest_file_index = i;
-                    break;
-                } else if file.modified > dest_file.modified {
-                    // Update needed
-                    let temp_diff = DiffFile {
-                        file: &file,
-                        action: ActionType::UpdateFile,
-                    };
-                    dest_file_index = i;
-                    diff_files.push(temp_diff);
-
-                    break;
-                }
-            }
-        }
-
-        if !file_exists_already {
-            let temp_diff = DiffFile {
-                file: &file,
-                action: ActionType::CopyFile,
-            };
-
-            diff_files.push(temp_diff);
-        } else {
-            destination_files.remove(dest_file_index);
-        }
-    }
+    let (destination_files_add, mut diff_files_add) = make_diff(&source_files, destination_files.to_vec());
+    diff_files.append(&mut diff_files_add);
+    destination_files = destination_files_add.to_vec();
 
     for dest_file in &destination_files {
         let temp_diff = DiffFile {
             file: &dest_file,
             action: ActionType::DeleteFile,
+            is_directory: Path::new(&dest_file.path).is_dir(),
         };
 
         diff_files.push(temp_diff);
@@ -113,11 +76,18 @@ fn main() {
         match diff.action {
             ActionType::CopyFile | ActionType::UpdateFile => {
                 let new_path = Path::new(destination).join(&diff.file.file_name);
-                match fs::copy(&diff.file.path, &new_path) {
-                    Ok(_) => println!("Successfully copied: {:?}", diff.file.path),
-                    Err(err) => println!("Error: {}", err),
-                }
 
+                if diff.is_directory {
+                    match fs::create_dir(&new_path) {
+                        Ok(_) => println!("Successfully made a new dir: {:?}", new_path),
+                        Err(err) => println!("Error: {}", err),
+                    } 
+                } else {
+                    match fs::copy(&diff.file.path, &new_path) {
+                        Ok(_) => println!("Successfully copied: {:?}", diff.file.path),
+                        Err(err) => println!("Error: {}", err),
+                    }
+                }
                 // Set the accessed time and modified time to be the same as on the original file
                 match filetime::set_file_times(&new_path, diff.file.access, diff.file.modified) {
                     Ok(_) => println!("Successfully modified 'accessed' and 'modified' times"),
@@ -175,4 +145,61 @@ fn read_files(directory: &Path) -> Vec<SyncFile> {
     }
 
     result_vec
+}
+
+fn make_diff<'a>(source_files: &'a Vec<SyncFile>, _destination_files: Vec<SyncFile>) -> (Vec<SyncFile>, Vec<DiffFile<'a>>) {
+
+    let mut diff_files = Vec::new();
+    let mut destination_files = _destination_files.to_vec();
+    // Check the differences between the source folder and the destination
+    for file in source_files {
+
+        let mut file_exists_already = false;
+        let mut dest_file_index = 0;
+        let is_directory = Path::new(&file.path).is_dir();
+
+        println!("Checking file: {:?}", &file.path);
+
+        // Check if the same file is already in destination
+        for (i, dest_file) in destination_files.iter().enumerate() {
+            if dest_file.file_name == file.file_name {
+
+                file_exists_already = true;
+
+                // check the modified times and sizes
+                if dest_file.size == file.size && dest_file.modified == file.modified {
+                    println!("No changes needed: {:?}", &dest_file.path);
+                    dest_file_index = i;
+                    break;
+                } else if file.modified > dest_file.modified {
+                    // Update needed
+                    let temp_diff = DiffFile {
+                        file: &file,
+                        action: ActionType::UpdateFile,
+                        is_directory: is_directory,
+                    };
+                    dest_file_index = i;
+                    diff_files.push(temp_diff);
+
+                    break;
+                }
+            }
+        }
+
+        if !file_exists_already {
+
+            let temp_diff = DiffFile {
+                file: &file,
+                action: ActionType::CopyFile,
+                is_directory: is_directory,
+            };
+
+            diff_files.push(temp_diff);
+        } else {
+            destination_files.remove(dest_file_index);
+        }
+    }
+
+    (destination_files, diff_files)
+
 }
